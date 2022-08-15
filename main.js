6c51e0cc3659600155f979c19709075ec19945dc
kevinsherrell <k.sherrell.1028@gmail.com>
2022-08-15 01:21:49 -0500

    detailed pokemon information

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
-import {loadAllBasic} from "./pokemon.js";
+import {loadAllBasic, getDetail} from "./pokemon.js";
+import {Home} from './home.js';
+
+const app = document.querySelector("#app");
+let pkmn = [];
+
 // Pokemon OG
 
 // Loads the initial 151 Pokémon and save them to local storage. If data already exists, existing data will be used instead.
 window.addEventListener('load', function () {
     if (window.localStorage.getItem("pokemonData") === null) {
+        // Load basic pokemon information
         loadAllBasic()
             .then(data => {
-                console.log(data);
-                window.localStorage.setItem('pokemonData', JSON.stringify(data));
+                // Use data from loadAllBasic to query for more detailed information on each pokemon.
+                data.forEach(pokemon => {
+                    getDetail(pokemon.name)
+                        .then(data => {
+                            // Add new data to array called pkmn
+                            pkmn.push(data);
+                            console.log(data);
+                        })
+                        .catch(err => console.log(err))
+                        .finally(() => {
+                            console.log(pkmn);
+                            // set pkmn to local storage
+                            window.localStorage.setItem('pokemonData', JSON.stringify(pkmn))
+                        })
+                })
+
             })
+
+            .catch(err => {
+                console.log(err);
+            })
+        // console.log(pokemonData);
     }
+
+    app.innerHTML = Home();
 })
+
+// const pokemonData = JSON.parse(window.localStorage.getItem('pokemonData'));
+// console.log(Array.isArray(pokemonData));
+// pokemonData.map(pokemon => {
+//     const newPokemon = document.createElement("div")
+//     newPokemon.textContent = pokemon.name;
+//     document.querySelector('.screen').append(newPokemon);
+// })
+
+/*
+* Brainstorm
+*
+* -- Create modular html5 templates via javascript
+* ----
+* */
--- pokemon.js
+++ pokemon.js
@@ -1,16 +1,16 @@
 
 // Contains all get requests for Poke API
  const loadAllBasic = () => {
-    return fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    return fetch("https://pokeapi.co/api/v2/pokemon-form?limit=151&offset=0")
         .then((response) => response.json())
         .then((data) => data.results);
 
 }
 // gets all details for selected pokemon
  const getDetail = (pokemon) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    return fetch(`https://pokeapi.co/api/v2/pokemon-form/${pokemon}`)
         .then((response) => response.json())
-        .then((data) => data.results)
+        .then((data) => data)
 }
 
 export {loadAllBasic, getDetail}
